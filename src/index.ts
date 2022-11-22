177fde5ff03756a38372384b9569eae8384fe2f8
Fluturecode <fluturecode@gmail.com>
2022-11-21 20:39:15 -0500

    added initial setup and initializeKeypair function

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,34 @@
-async funciton intializeKeypair(connection: Web3.Connection): Promise<Web3.Keypair> {
+import * as Web3 from '@solana/web3.js';
+import * as fs from 'fs';
+import dotenv from 'dotenv';
+dotenv.config();
+
+async function initializeKeypair(connection: Web3.Connection): Promise<Web3.Keypair> {
     if (!process.env.PRIVATE_KEY) {
-        console.log('Generating new keypair... 🗝️');
-        const signer = Web3.Keypair.generate();
+      console.log('Generating new keypair... 🗝️');
+      const signer = Web3.Keypair.generate();
 
-        console.log('Creating .env file');
-        fs.writeFileSync('.env'. `PRIVATE_KEY=[${signer.secretKey.toString()}]`);
+      console.log('Creating .env file');
+      fs.writeFileSync('.env', `PRIVATE_KEY=[${signer.secretKey.toString()}]`);
 
-        return signer;
+      return signer;
     }
 
     const secret = JSON.parse(process.env.PRIVATE_KEY ?? '') as number[];
-    const secretKey = Uint8array.from (secret);
+    const secretKey = Uint8Array.from(secret);
     const keypairFromSecret = Web3.Keypair.fromSecretKey(secretKey);
     return keypairFromSecret;
+  }
+
+  async function main() {
 }
 
-async function main() {}
-
 main()
-    .then(() => {
-        console.log("Finished successfully")
-        process.exit(0)
-    })
-    .catch((error) => {
-        console.log(error)
-        process.exit(1)
-    })
+  .then(() => {
+    console.log('Finished successfully');
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });

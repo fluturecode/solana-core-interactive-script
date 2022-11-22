cc847faf6a8c72e587a31960da30daf7a0db898e
Fluturecode <fluturecode@gmail.com>
2022-11-21 21:33:04 -0500

    added pingProgram function

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,40 @@ import * as fs from 'fs';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const PROGRAM_ID = new Web3.PublicKey("ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa")
+const PROGRAM_DATA_PUBLIC_KEY = new Web3.PublicKey("Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod")
+
+async function pingProgram(connection: Web3.Connection, payer: Web3.Keypair) {
+    const transaction = new Web3.Transaction()
+    const instruction = new Web3.TransactionInstruction({
+
+    keys: [
+        {
+            pubkey: PROGRAM_DATA_PUBLIC_KEY,
+            isSigner: false,
+            isWritable: true
+        }
+    ],
+
+    programId: PROGRAM_ID
+
+    })
+
+    transaction.add(instruction)
+    const transactionSignature = await Web3.sendAndConfirmTransaction(connection, transaction, [payer])
+
+    console.log(
+        `Transaction https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
+    )
+}
+
 async function main() {
     const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'));
     const signer = await initializeKeypair(connection);
 
     console.log("Public key:", signer.publicKey.toBase58());
+
+    await pingProgram(connection, signer)
 }
 
 async function initializeKeypair(connection: Web3.Connection): Promise<Web3.Keypair> {

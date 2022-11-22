3e4e9bd6783288dd7cdf6dd6dd2a9a6841f300e3
Fluturecode <fluturecode@gmail.com>
2022-11-21 21:18:40 -0500

    fixed errors and whitespace

--- src/index.ts
+++ src/index.ts
@@ -3,65 +3,63 @@ import * as fs from 'fs';
 import dotenv from 'dotenv';
 dotenv.config();
 
+async function main() {
+    const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'));
+    const signer = await initializeKeypair(connection);
+
+    console.log("Public key:", signer.publicKey.toBase58());
+}
+
 async function initializeKeypair(connection: Web3.Connection): Promise<Web3.Keypair> {
     if (!process.env.PRIVATE_KEY) {
-      console.log('Generating new keypair... 🗝️');
-      const signer = Web3.Keypair.generate();
+        console.log('Generating new keypair... 🗝️');
+        const signer = Web3.Keypair.generate();
 
-      console.log('Creating .env file');
-      fs.writeFileSync('.env', `PRIVATE_KEY=[${signer.secretKey.toString()}]`);
+        console.log('Creating .env file');
+        fs.writeFileSync('.env', `PRIVATE_KEY=[${signer.secretKey.toString()}]`);
 
-      return signer;
+        return signer;
     }
 
     const secret = JSON.parse(process.env.PRIVATE_KEY ?? '') as number[];
     const secretKey = Uint8Array.from(secret);
     const keypairFromSecret = Web3.Keypair.fromSecretKey(secretKey);
     return keypairFromSecret;
-  }
-
-  async function main() {
-    const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'));
-    const signer = await initializeKeypair(connection);
-
-    console.log('Public key:', signer.publicKey.toBase58());
 }
 
-main()
-  .then(() => {
-    console.log('Finished successfully');
-    process.exit(0);
-  })
-  .catch((error) => {
-    console.log(error);
-    process.exit(1);
-  });
-
-
-  async function airdropSolIfNeeded(
+async function airdropSolIfNeeded(
     signer: Web3.Keypair,
     connection: Web3.Connection
-  ) {
+) {
     const balance = await connection.getBalance(signer.publicKey);
     console.log('Current balance is', balance / Web3.LAMPORTS_PER_SOL, 'SOL');
 
     if (balance / Web3.LAMPORTS_PER_SOL < 1) {
 
-      console.log('Airdropping 1 SOL');
-      const airdropSignature = await connection.requestAirdrop(
-        signer.publicKey,
-        Web3.LAMPORTS_PER_SOL
-      );
+        console.log('Airdropping 1 SOL');
+        const airdropSignature = await connection.requestAirdrop(
+            signer.publicKey,
+            Web3.LAMPORTS_PER_SOL
+        );
 
-      const latestBlockhash = await connection.getLatestBlockhash();
+        const latestBlockhash = await connection.getLatestBlockhash();
 
-      await connection.confirmTransaction({
-        blockhash: latestBlockhash.blockhash,
-        lastValidBlockHeight: latestBlockhash.lastValidBlockHeight,
-        signature: airdropSignature,
-      });
+        await connection.confirmTransaction({
+            blockhash: latestBlockhash.blockhash,
+            lastValidBlockHeight: latestBlockhash.lastValidBlockHeight,
+            signature: airdropSignature,
+        });
 
-      const newBalance = await connection.getBalance(signer.publicKey);
-      console.log('New balance is', newBalance / Web3.LAMPORTS_PER_SOL, 'SOL');
+        const newBalance = await connection.getBalance(signer.publicKey);
+        console.log('New balance is', newBalance / Web3.LAMPORTS_PER_SOL, 'SOL');
     }
-  }
+}
+main()
+    .then(() => {
+    console.log('Finished successfully');
+    process.exit(0);
+    })
+    .catch((error) => {
+    console.log(error);
+    process.exit(1);
+    });

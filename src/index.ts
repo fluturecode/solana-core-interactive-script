4b4753852962187444e6b7943e1dd5252009f3d5
Fluturecode <fluturecode@gmail.com>
2022-11-21 20:31:45 -0500

    added initial setup and initializeKeypair function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
+async funciton intializeKeypair(connection: Web3.Connection): Promise<Web3.Keypair> {
+    if (!process.env.PRIVATE_KEY) {
+        console.log('Generating new keypair... 🗝️');
+        const signer = Web3.Keypair.generate();
+
+        console.log('Creating .env file');
+        fs.writeFileSync('.env'. `PRIVATE_KEY=[${signer.secretKey.toString()}]`);
+
+        return signer;
+    }
+
+    const secret = JSON.parse(process.env.PRIVATE_KEY ?? '') as number[];
+    const secretKey = Uint8array.from (secret);
+    const keypairFromSecret = Web3.Keypair.fromSecretKey(secretKey);
+    return keypairFromSecret;
+}
+
 async function main() {}
 
 main()
